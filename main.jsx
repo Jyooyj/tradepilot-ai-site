7308ad76fab27bf54f80e224a63f63352024d1a1
Jyooyj <272114508@qq.com>
2026-05-06 11:56:45 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import AuthGate from './AuthGate.jsx';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import AuthGate from "./AuthGate.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <AuthGate>
-    <App />
-  </AuthGate>
+createRoot(document.getElementById("root")).render(
+  <ErrorBoundary>
+    <AuthGate>
+      <App />
+    </AuthGate>
+  </ErrorBoundary>
 );
