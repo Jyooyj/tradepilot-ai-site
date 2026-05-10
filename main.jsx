ad5bd9ac74de7a73699fc19e55b72cc338a2cc1f
Jyooyj <272114508@qq.com>
2026-05-10 23:39:41 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import AuthGate from "./AuthGate.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ErrorBoundary>
+    <AuthGate>
+      <App />
+    </AuthGate>
+  </ErrorBoundary>
 );
